712384374608c6d08643ec3979e0a87309ccfe90
Mark-Randria <randrianjafymark2.0@gmail.com>
2024-12-14 12:44:59 +0300

    test: add failing test for UsersService.findOneBy method

--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { Repository } from 'typeorm';
+import { UserEntity } from './user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const mockUserRepository = () => ({
+  findOneBy: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+});
 
 describe('UsersService', () => {
   let service: UsersService;
+  let userRepository: Partial<Repository<UserEntity>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useFactory: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    userRepository = module.get<Repository<UserEntity>>(
+      getRepositoryToken(UserEntity),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should find a user by ID', async () => {
+    const mockUser = {
+      id: 1,
+      firstname: 'test',
+      lastname: 'test',
+      email: 'test@example.com',
+      isAdmin: false,
+    } as UserEntity;
+    (userRepository.findOneBy as jest.Mock).mockResolvedValue(mockUser);
+
+    const result = await service.findUserById(1);
+
+    expect(userRepository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+    expect(result).toEqual(mockUser);
   });
 });
