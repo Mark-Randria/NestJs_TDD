c5aa7481c29d64e43fb381ec01ed9a920a8154e8
Mark-Randria <randrianjafymark2.0@gmail.com>
2024-12-14 14:43:43 +0300

    test: add failing test for UsersService.create and .find

--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -3,12 +3,32 @@ import { UsersService } from './users.service';
 import { Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { IUser } from './interfaces/user.interface';
 
 const mockUserRepository = () => ({
   findOneBy: jest.fn(),
+  find: jest.fn(),
   create: jest.fn(),
   save: jest.fn(),
 });
+
+const oneUser = {
+  id: 1,
+  firstname: 'test',
+  lastname: 'test',
+  email: 'test@example.com',
+  isAdmin: false,
+} as UserEntity;
+
+const otherUser = {
+  id: 2,
+  firstname: 'test',
+  lastname: 'test',
+  email: 'test@example.com',
+  isAdmin: true,
+} as UserEntity;
+
+const listUser: IUser[] = [oneUser, otherUser];
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -33,21 +53,44 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(userRepository).toBeDefined();
   });
 
   it('should find a user by ID', async () => {
-    const mockUser = {
-      id: 1,
-      firstname: 'test',
-      lastname: 'test',
-      email: 'test@example.com',
-      isAdmin: false,
-    } as UserEntity;
-    (userRepository.findOneBy as jest.Mock).mockResolvedValue(mockUser);
+    (userRepository.findOneBy as jest.Mock).mockResolvedValue(oneUser);
 
     const result = await service.findUserById(1);
 
     expect(userRepository.findOneBy).toHaveBeenCalledWith({ id: 1 });
-    expect(result).toEqual(mockUser);
+    expect(result).toEqual(oneUser);
+  });
+
+  it('should find a list of all user', async () => {
+    (userRepository.find as jest.Mock).mockResolvedValue(listUser);
+
+    const result = await service.findAll();
+
+    expect(userRepository.find).toHaveBeenCalled();
+    expect(result).toEqual(listUser);
+  });
+
+  it('should return the saved user', async () => {
+    const userDTO = {
+      firstname: 'test',
+      lastname: 'test',
+      email: 'test@example.com',
+      password: 'secret',
+    };
+    const savedUser = { id: 1, isAdmin: false, ...userDTO } as UserEntity;
+
+    (userRepository.create as jest.Mock).mockReturnValue(savedUser);
+    (userRepository.save as jest.Mock).mockReturnValue(savedUser);
+
+    const result = await service.createUser(userDTO);
+
+    expect(userRepository.create).toHaveBeenCalledWith(userDTO);
+    expect(userRepository.save).toHaveBeenCalledWith(savedUser);
+
+    expect(result).toEqual(savedUser);
   });
 });
