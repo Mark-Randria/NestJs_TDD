2f1c5c966491cbc98cd17725d30f640f9aa55dbd
Mark-Randria <randrianjafymark2.0@gmail.com>
2024-12-14 13:46:02 +0300

    fix: database connection readjustment for unit test

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,14 +1,16 @@
-import { Injectable, Inject } from '@nestjs/common';
-import { DataSource, Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
+import { IUser } from './interfaces/user.interface';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
   // 1st useCase: usage of typeORMConnection without database providers
-  // constructor(
-  //   @InjectRepository(UserEntity)
-  //   private readonly userRepository: Repository<UserEntity>,
-  // ) {}
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
   // 2nd useCase: usage of userProviders with database providers
   // constructor(
@@ -17,8 +19,12 @@ export class UsersService {
   // ) {}
 
   // 3rd useCase: usage of different datasource injection using providers
-  private userRepository: Repository<UserEntity>;
-  constructor(@Inject('TYPEORM_DATA_SOURCE') private dataSource: DataSource) {
-    this.userRepository = this.dataSource.getRepository(UserEntity);
+  // private userRepository: Repository<UserEntity>;
+  // constructor(@Inject('TYPEORM_DATA_SOURCE') private dataSource: DataSource) {
+  //   this.userRepository = this.dataSource.getRepository(UserEntity);
+  // }
+
+  async findUserById(id: number): Promise<IUser> {
+    return await this.userRepository.findOneBy({ id });
   }
 }
