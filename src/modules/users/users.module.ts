51064f1fea827556ae110482a9164482108af245
Mark-Randria <randrianjafymark2.0@gmail.com>
2024-12-14 14:44:18 +0300

    fix: fix dependencies issues

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { usersProviders } from './users.providers';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user.entity';
 import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [TypeOrmModule.forFeature([UserEntity]), DatabaseModule],
   controllers: [UsersController],
   providers: [UsersService, ...usersProviders],
 })
